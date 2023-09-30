69cb955c58714cd0432ecbedf8b728139efe2e8e
mxkuhn <mxkuhn7@gmail.com>
2023-09-30 12:23:29 -0500

    commiy

--- Oauth/urls.py
+++ Oauth/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('oauth/', include('social_django.urls', namespace='social'))
+    path('', views.login, include('social_django.urls', namespace='social'))
 ]
