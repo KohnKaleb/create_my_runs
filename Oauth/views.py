391758c10bf9437f7277bac895471f44131884b5
kalebkohn <kjkohn@wisc.edu>
2023-10-01 19:33:20 -0500

    Additions to database on login (tehe)

--- Oauth/views.py
+++ Oauth/views.py
@@ -4,7 +4,7 @@ import requests
 import polyline
 import json
 from django.conf import settings
-
+from .models import Activities
 
 def login(request):
     return render(request, 'login.html')
@@ -26,8 +26,13 @@ def strava_callback(request, path=''):
         response = requests.get(url, headers=headers).json()
         MAPBOX_KEY = settings.MAPBOX_KEY
         
-        
         if response:
+            for activity in response:
+                if activity['map'] == None or activity['map']['summary_polyline'] == None:
+                    continue
+                add_to_db(activity)
+            activity['map']['summary_polyline'] = polyline.decode(activity['map']['summary_polyline'], geojson=True)
+            activity['map']['summary_polyline'] = [list(tup) for tup in activity['map']['summary_polyline']]
             poly = response[0]['map']['summary_polyline']
             coords = polyline.decode(poly, geojson=True)
             coords = [list(tup) for tup in coords]
@@ -56,3 +61,11 @@ def exchange_code_for_token(authorization_code):
 def strava_redirect(request):
     route = 'https://www.strava.com/oauth/authorize?client_id=' + str(settings.STRAVA_CLIENT_ID) + '&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=activity:read_all'
     return redirect(route)
+
+
+def add_to_db(activity):
+    if not Activities.objects.filter(athlete_id=activity["athlete"]["id"], activity_id=activity["id"]).exists():
+            entry = Activities(athlete_id=activity["athlete"]["id"], activity_id=activity["id"], name=activity["name"], distance=activity["distance"], total_elevation_gain=activity["total_elevation_gain"], type=activity["type"], location_city=activity["location_city"], location_state=activity["location_state"], location_country=activity["location_country"], mapid=activity["map"]["id"], mappolyline=activity["map"]["summary_polyline"], mapresource_state=activity["map"]["resource_state"], upload_id=activity["upload_id"])
+            entry.save()
+    else:
+        pass
